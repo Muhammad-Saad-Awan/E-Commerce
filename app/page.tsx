ace3a99ab2d1587130e3bfcbe1828b8bb8fdc5b0
Muhammad Saad <148006898+ssaadd-bot@users.noreply.github.com>
2024-12-06 18:40:00 +0500

    First

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+ 
+export default function main(){
+  return(
+    <div className="mt-36 ">
+      <hr className=""/>
+     <div className=" mx-[135px] pt-8     ">
+      <div className=" flex  flex-col  gap-y-[16px]">
+      <div>Womans Fashion</div>
+      <div>Mens Fashion</div>
+      <div>Electronics</div>
+      <div>Home and Lifestyle</div>
+      <div>Medicine</div>
+      <div>Sports and Outdoor</div>
+      <div>Babys and Toys</div>
+      <div>Groceries and Pets</div>
+      <div>Health and Beauty</div>
+      </div>
+     </div>
+    </div>
+  )
+}
